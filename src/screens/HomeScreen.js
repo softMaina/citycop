3bc6c526fee7048133aee8a7d75ebc9c1c1d5ebb
softMaina <allancodes@gmail.com>
2019-09-08 22:56:23 +0300

    add sample appbar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Appbar} from 'react-native-paper';
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        />
+      <View style={{flex: 1}}>
+        <Appbar style={styles.bottom}>
+          <Appbar.Action
+            icon="archive"
+            onPress={() => console.log('Pressed archive')}
+          />
+          <Appbar.Action
+            icon="mail"
+            onPress={() => console.log('Pressed mail')}
+          />
+          <Appbar.Action
+            icon="label"
+            onPress={() => console.log('Pressed label')}
+          />
+          <Appbar.Action
+            icon="delete"
+            onPress={() => console.log('Pressed delete')}
+          />
+        </Appbar>
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -32,5 +53,10 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+  },
 });
-
